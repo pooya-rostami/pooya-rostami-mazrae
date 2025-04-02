4a39098953f93bfb42409b8d783d940c86f08fe0
pooya rostami <pooyarostami@Pooya-Work-MacBook.local>
2025-04-02 15:52:03 +0200

    update

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -7,7 +7,7 @@ const config: Config = {
   tagline: "Personal website/blog",
 
   // Set the production url of your site here
-  url: "https://github.com",
+  url: "https://pooya-rostami.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
